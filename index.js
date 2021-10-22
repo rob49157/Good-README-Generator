33f829833dc5b2eb65442c3ec187634fc0eddd87
rob49157 <robertopaipo@hotmail.com>
2021-10-22 18:14:41 -0400

    questions

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const fs= require("fs")
 
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+.prompt([
+    // display as title of readme
+    {type:'input',
+    message:" Enter name of Repository:",
+    name: 'repo name'},
+    
+    // licences
+    // github user name
+    {type: 'input',
+     message: ' Enter github user name:',
+      name: 'gitusername' },
+    // enter email address
+    {type: 'input',
+    message:' Enter email:',
+    name: "email"}
+
+
+
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
