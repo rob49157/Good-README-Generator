47edc555c3bcb6704260497153dafce27960612a
rob49157 <robertopaipo@hotmail.com>
2021-10-24 21:31:53 -0400

    readme file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
-const fs= require("fs")
+const fs= require("fs");
+const { error } = require('console');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
 inquirer
 .prompt([
     // display as title of readme
@@ -12,6 +13,14 @@ inquirer
     name: 'repo name'},
     
     // licences
+
+    {
+      type:'list',
+      message: 'select licenses permitted:',
+      name: 'licenses',
+      choices: ['Ansible','Bash', 'GIMP']
+
+    },
     // github user name
     {type: 'input',
      message: ' Enter github user name:',
@@ -24,12 +33,19 @@ inquirer
 
 
 ])
+]
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile('README.md',process.argv[2],questions,(err) => err ? console.log(err): console.log('success'))
+
+  }
+
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
